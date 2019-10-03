5261a5f356f58bd3b766e20eb7cc0d62b9de23e8
James Darling <james@abscond.org>
2019-10-03 13:22:10 +0100

    Add very basic user login
    Add Devise gem
    - removes the `registerable` module from the default settings, as for now users will be managed via the command line. To do this enter `User.create!(email: 'email@example.com', password: 'password', password_confirmation: 'password)`
    - Add current user email and logout button to header
    - authenticate users for the entire application in application controller
    Add FactoryBot for user factory
    Add login spec helper for future use

---
+++ spec/features/signing_in_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature 'Signing in', type: :feature do
+  scenario 'trying to view the home page without signing in' do
+    visit root_path
+    expect(current_path).to eq('/users/sign_in')
+    expect(page).to have_text('You need to sign in before continuing')
+  end
+
+  scenario 'succesfully' do
+    user = create(:user)
+    visit root_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+
+    expect(page).to have_text('Signed in successfully')
+  end
+
+  scenario 'with incorrect password' do
+    create(:user)
+    visit root_path
+    fill_in 'Email', with: 'wrong@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+
+    expect(page).to have_text('Invalid Email or password')
+  end
+
+  scenario 'signing out' do
+    login
+    click_button 'Logout'
+
+    expect(page).to have_text('You need to sign in before continuing')
+  end
+end
---
+++ spec/support/login_helpers.rb
@@ -0,0 +1,7 @@
+def login(user = nil)
+  user ||= create(:user)
+  visit root_path
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: 'password'
+  click_button 'Log in'
+end
