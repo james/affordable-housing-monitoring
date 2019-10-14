c39ba92b924d2f8d2f0c14bc7777980fe92db393
James Darling <james@abscond.org>
2019-10-14 15:22:20 +0100

    Add states to development model
    - Add acts_as_state_machine to manage state
    - Add validation to make sure column is present
    - Default to 'default' state

--- spec/models/development_spec.rb
+++ spec/models/development_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Development, type: :model do
+  describe '#state' do
+    it 'cannot be nil' do
+      development = build(:development, state: nil)
+      expect(development).to be_invalid
+    end
+
+    it 'cannot be blank' do
+      development = build(:development, state: '')
+      expect(development).to be_invalid
+    end
+
+    it 'cannot be an invalid state' do
+      development = build(:development, state: 'something-weird')
+      expect(development).to be_invalid
+    end
+
+    it 'can be a valid state' do
+      development = build(:development, state: 'draft')
+      expect(development).to be_valid
+    end
+  end
 end
