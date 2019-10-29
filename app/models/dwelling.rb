1af1b8c62db3c354cdcd14d71ec7aab0704ac19e
James Darling <james@abscond.org>
2019-10-29 16:28:22 +0000

    Audit addition of dwellings to a development is development not draft
    - Break changelog description into different partials for each type of change

--- app/models/dwelling.rb
+++ app/models/dwelling.rb
@@ -1,9 +1,18 @@
 class Dwelling < ApplicationRecord
   belongs_to :development
 
+  audited(
+    associated_with: :development,
+    if: :audit_changes?,
+    on: [:create],
+    comment_required: true
+  )
+
   TENURES = %w[open social intermediate].freeze
 
   validates :tenure, presence: true
   validates :habitable_rooms, presence: true
   validates :bedrooms, presence: true
+
+  delegate :audit_changes?, to: :development
 end
