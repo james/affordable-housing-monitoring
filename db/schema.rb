aa2662b43222c956fdab4cbef249bff7056bb095
James Darling <james@abscond.org>
2019-12-02 17:45:01 +0000

    Allow user to set a started_on date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_162020) do
+ActiveRecord::Schema.define(version: 2019_12_02_173904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_162020) do
     t.string "name"
     t.string "developer"
     t.date "agreed_on"
+    t.date "started_on"
   end
 
   create_table "dwellings", force: :cascade do |t|
--- spec/features/starting_a_development_spec.rb
+++ spec/features/starting_a_development_spec.rb
@@ -7,9 +7,13 @@ RSpec.feature 'Marking a development as started', type: :feature do
     click_link 'AP/2019/1234'
     expect(page).to have_content 'Agreed'
     click_link 'Mark as started'
+    fill_in 'Day', with: '30'
+    fill_in 'Month', with: '03'
+    fill_in 'Year', with: '2019'
     click_button 'Mark as started'
     expect(page).to have_content 'Development marked as started'
     development.reload
     expect(development.state).to eq('started')
+    expect(development.started_on).to eq('30/03/2019'.to_date)
   end
 end
