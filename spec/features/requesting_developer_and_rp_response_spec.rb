a56ef5c2497af3b71b07f36c6bf221eb621bcdac
James Darling <james@abscond.org>
2019-12-04 17:22:00 +0000

    Show RP response URL to case officer

--- spec/features/requesting_developer_and_rp_response_spec.rb
+++ spec/features/requesting_developer_and_rp_response_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature 'Requesting a developer completion response', type: :feature do
-  scenario 'when development is unconfirmed_completed and has incomplete dwelling' do
+RSpec.feature 'Requesting responses from RPs and developers', type: :feature do
+  scenario 'when development is unconfirmed_completed' do
     development = create(:development, state: 'unconfirmed_completed')
     login
     click_link 'AP/2019/1234'
@@ -9,11 +9,19 @@ RSpec.feature 'Requesting a developer completion response', type: :feature do
     expect(find_field('developer_response_url').value).to have_content(completion_response_form_development_path(development))
   end
 
+  scenario 'when development is partially_confirmed_completed' do
+    development = create(:development, state: 'partially_confirmed_completed')
+    login
+    click_link 'AP/2019/1234'
+    expect(page).to have_content 'Registered Provider response needed'
+    expect(find_field('rp_response_url').value).to have_content(rp_response_form_development_path(development))
+  end
+
   scenario 'when development is confirmed_completed' do
     create(:development, state: 'confirmed_completed')
     login
     click_link 'AP/2019/1234'
-    expect(page).to have_content 'The developer has responded with the full details for this development.'
+    expect(page).to have_content 'The developer and registered provider have responded with the full details for this development.'
   end
 
   scenario 'when development is not unconfirmed_completed' do
