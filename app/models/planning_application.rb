01129235f6f97ac9d201dc36665ad37730be8286
James Darling <james@abscond.org>
2019-12-10 12:07:02 +0000

    Stop audits stopping us from being able to destroy planning applications

--- app/models/planning_application.rb
+++ app/models/planning_application.rb
@@ -1,4 +1,6 @@
 class PlanningApplication < ApplicationRecord
   belongs_to :development
   validates :application_number, presence: true
+
+  has_many :affordable_housing_audits, dependent: :nullify
 end
