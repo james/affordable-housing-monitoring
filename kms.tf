db4da472b4268389a13a8a258089dbcf189b9236
Chris Wright <chris@dxw.com>
2021-01-26 09:30:09 +0000

    (Chore) Remove terraform files
    * These resources are now created by Dalmatian

--- kms.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_kms_key" "ssm" {
-  description             = "SSM key ${terraform.workspace} ${local.app_name}"
-  deletion_window_in_days = 10
-}
-
-resource "aws_kms_alias" "ssm" {
-  name          = "alias/ssm-${terraform.workspace}-${local.app_name}"
-  target_key_id = "${aws_kms_key.ssm.key_id}"
-}
